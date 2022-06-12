8d3c126290d3611cb2a2aa6c1604c79c13c285d4
BennoAlif <bennoalif41@gmail.com>
2022-06-12 10:45:24 +0700

    Added delete and update features

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,20 +1,41 @@
-const Post = require("../models/post");
+const Post = require('../models/post');
 
 module.exports = {
-    index: async (req, res) => {
-        const posts = await Post.findAll();
-        return res.render('post/index',{ 
-            posts 
-        });
-    },
+  index: async (req, res) => {
+    const posts = await Post.findAll();
+    return res.render('post/index', {
+      posts,
+    });
+  },
 
-    create: async (req, res) => {
-        return res.render('post/create');
-    },
+  details: async (req, res) => {
+    const post = await Post.findByPk(req.params.id);
+    return res.render('post/details', { post });
+  },
 
-    store: async (req, res) => {
-        await Post.create(req.body);
+  create: async (req, res) => {
+    return res.render('post/create');
+  },
 
-        return res.redirect('/posts');
-    },
+  store: async (req, res) => {
+    await Post.create(req.body);
+
+    return res.redirect('/posts');
+  },
+
+  delete: async (req, res) => {
+    await Post.destroy({ where: { id: req.params.id } });
+    return res.redirect('/posts');
+  },
+
+  edit: async (req, res) => {
+    const post = await Post.findByPk(req.params.id);
+    return res.render('post/edit', { post });
+  },
+
+  update: async (req, res) => {
+    await Post.update(req.body, { where: { id: req.params.id } });
+    // const post = await Post.findByPk(req.params.id);
+    return res.redirect('/posts');
+  },
 };
